a0a2c54f580649fab179f73f3cee078448c31ea6
torkil95 <145580009+torkil95@users.noreply.github.com>
2023-12-16 18:35:35 +0100

    Oppdatert informasjon

--- javascript/components.js
+++ javascript/components.js
@@ -3,7 +3,7 @@ class MyHeader extends HTMLElement {
         this.innerHTML =  `
         <header>
             <div class="header">
-                <h1><a href="index.html">legehjelp</a></h1>
+                <h1 class="logo"><a href="index.html">legehjelp</a></h1>
                 <button class="navbar-toggle" onclick="hideShow()">
                     <i class="fa fa-bars"></i>
                 </button>
@@ -12,7 +12,7 @@ class MyHeader extends HTMLElement {
                     <li class="nav-item"><a class="nav-link" href="index.html">Normalfraser</a></li>
                     <li class="nav-item"><a class="nav-link" href="gentamycin.html">Genta-kalkulator</a></li>
                     <li class="nav-item"><a class="nav-link" href="index.html">Prosjekter</a></li>
-                    <li class="nav-item"><a class="nav-link" href="om_oss.html">Om oss</a></li>
+                    <li class="nav-item"><a class="nav-link" href="om_oss.html">Om legehjelp</a></li>
                 </nav>
             </div>
             <nav id="nav-mobile" style="display: none;">
@@ -20,7 +20,7 @@ class MyHeader extends HTMLElement {
                 <li class="nav-item"><a class="nav-link" href="index.html">Normalfraser</a></li>
                 <li class="nav-item"><a class="nav-link" href="gentamycin.html">Genta-kalkulator</a></li>
                 <li class="nav-item"><a class="nav-link" href="index.html">Prosjekter</a></li>
-                <li class="nav-item"><a class="nav-link" href="om_oss.html">Om oss</a></li> 
+                <li class="nav-item"><a class="nav-link" href="om_oss.html">Om legehjelp</a></li> 
             </nav>
         </header>
         `
