31fae9fc9d77b37b7c091a9ebc09ed8365d20e57
torkil95 <145580009+torkil95@users.noreply.github.com>
2023-12-19 00:55:37 +0100

    foden

--- uferdig/fodsel.js
+++ uferdig/fodsel.js
@@ -1,58 +1,97 @@
+//dagens dato
+var now = new Date();
+var datetime = now.toLocaleString();
+document.getElementById("datetime").innerHTML = datetime;
+
 //her må jeg legge inn en array for hver av pasientene. 
 const pasient1 = {
     fornavn: "Kari", 
     etternavn: "Nordmann", 
-    fodselsnummer: 11223344444,
+    fodselsnummerMor: 11223344444,
+    sivilstatus: "gift",
     alder: 25,
-    statsborgerskap: "Norge"
+    statsborgerskapMor: "Norge"
 }
 
 const pasient2 = {
     fornavn: "Helene", 
     etternavn: "Helensrud", 
-    fodselsnummer: 22222222222,
+    //fodselsnummerMor: 11223344444,
+    sivilstatus: "samboer",
     alder: 28,
-    statsborgerskap: "Norge"
+    statsborgerskapMor: "Norge"
 }
 
 const pasient3 = {
     fornavn: "Gro", 
     etternavn: "Rud", 
-    fodselsnummer: 33333333333,
+    fodselsnummerMor: 33333333333,
     alder: 30,
-    statsborgerskap: "Sverige"
+    statsborgerskapMor: "Sverige"
 }
 
-
-//aktivere pasient (nå kun til å si noe tekst)
+//aktivere pasient 
 function AktiverPasient() {
     Pasient = document.querySelector("#Pasient").value;
   
     if (Pasient == "Velg Pasient") {
         document.querySelector("#PasientAktiv").innerHTML = "det mangler kjønn";
     }
-    if (Pasient == "Pasient #1") {
+    if (Pasient != "Velg Pasient") {
+        //velg hvilken pasient som er aktiv og sett variabelen "PasientAktiv".
+        if (Pasient == "Pasient #1") {
+            PasientAktiv = pasient1;
+        }
+        else if (Pasient == "Pasient #2") {
+            PasientAktiv = pasient2;
+        }
+        else if (Pasient == "Pasient #3") {
+            PasientAktiv = pasient3;
+        }
+
+        //Vis pasientinformasjon under "aktiv pasient"
         document.querySelector("#PasientAktiv").innerHTML = 
-        "Valgt pasient: " + Pasient + "<br>" + 
-        "Navn: " + pasient1.fornavn + " " + pasient1.etternavn + "<br>" +
-        "Fødselsnummer " + pasient1.fodselsnummer + "<br>" +
-        "Statsborgerskap " + pasient1.statsborgerskap;
-    }
-    if (Pasient == "Pasient #2") {
-        document.querySelector("#PasientAktiv").innerHTML = 
-        "Valgt pasient: " + Pasient + "<br>" + 
-        "Navn: " + pasient2.fornavn + " " + pasient2.etternavn + "<br>" +
-        "Fødselsnummer " + pasient2.fodselsnummer + "<br>" +
-        "Statsborgerskap " + pasient2.statsborgerskap;
-    }
-    if (Pasient == "Pasient #3") {
-        document.querySelector("#PasientAktiv").innerHTML = 
-        "Valgt pasient: " + Pasient + "<br>" + 
-        "Navn: " + pasient3.fornavn + " " + pasient3.etternavn + "<br>" +
-        "Fødselsnummer " + pasient3.fodselsnummer + "<br>" +
-        "Statsborgerskap " + pasient3.statsborgerskap;
+        "<b>Valgt pasient: </b>" + Pasient + "<br>" + 
+        "<b>Navn: </b>" + PasientAktiv.fornavn + " " + PasientAktiv.etternavn + "<br>" +
+        "<b>Fødselsnummer: </b>" + PasientAktiv.fodselsnummerMor + "<br>" +
+        "<b>Statsborgerskap: </b>" + PasientAktiv.statsborgerskapMor;
+
+        //LEGG INN INFO I SKJEMA
+        //Fodselsnummer mor
+        if (PasientAktiv.fodselsnummerMor == "" || PasientAktiv.fodselsnummerMor === undefined) {
+            document.querySelector("#FødselsnummerMor").style.borderColor = "red";
+            document.querySelector("#FødselsnummerMor").style.background = "white";
+        }
+        else {
+            document.querySelector("#FødselsnummerMor").value = PasientAktiv.fodselsnummerMor;
+            document.querySelector("#FødselsnummerMor").style.borderColor = "black";
+            document.querySelector("#FødselsnummerMor").style.background = "#e8f0fe";
+        }
+
+        //Sivilstatus mor
+        if (PasientAktiv.sivilstatus == "" || PasientAktiv.sivilstatus === undefined) {
+            //document.querySelector("#Sivilstatus").style.borderColor = "red";
+            //document.querySelector("#Sivilstatus").style.background = "white";
+        }
+        else if (PasientAktiv.sivilstatus == "gift") {
+            document.querySelector("#SivilstatusGift").checked = true;
+        }
+        else if (PasientAktiv.sivilstatus == "samboer") {
+            document.querySelector("#SivilstatusSamboer").checked = true;
+        }
+
+
+        //Statsborgerskap mor
+        if (PasientAktiv.statsborgerskapMor == "" || PasientAktiv.statsborgerskapMor === undefined) {
+            document.querySelector("#StatsborgerskapMor").style.borderColor = "red";
+            document.querySelector("#StatsborgerskapMor").style.background = "white";
+        }
+        else {
+            document.querySelector("#StatsborgerskapMor").value = PasientAktiv.statsborgerskapMor;
+            document.querySelector("#StatsborgerskapMor").style.borderColor = "black";
+            document.querySelector("#StatsborgerskapMor").style.background = "#e8f0fe";
+        }
     }
   }
 
   //notater
-  
